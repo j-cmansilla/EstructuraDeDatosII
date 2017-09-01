c83ddc6c2d6ecf830ca1856e83f85ec6f793c215
Unknown <jcmansillam@correo.url.edu.gt>
2017-09-01 12:24:11 -0600

    Instrucciones
    Se agregaron las instrucciones al programa

--- Lab1 compresion de datos/Lab1-Compresion-de-Datos/Lab1-Compresion-de-Datos/Program.cs
+++ Lab1 compresion de datos/Lab1-Compresion-de-Datos/Lab1-Compresion-de-Datos/Program.cs
@@ -11,22 +11,39 @@ namespace Lab1_Compresion_de_Datos
     {
         static void Main(string[] args)
         {
-            Dictionary<string,int> Characters = new Dictionary<string, int>();
+            string filePath = string.Empty;
+            Dictionary<string, int> Characters = new Dictionary<string, int>();
             UploadFile UF = new UploadFile();
             AnalyzeTxt AT = new AnalyzeTxt();
-            List<string> listaStrings = new List<string>();
-            Console.Write("Type file path>");
-            //List<byte[]> txtlines = UF.OpenFile(Console.ReadLine());
-            string[] array = UF.OpenFileString(Console.ReadLine());
-            for (int i = 0; i < array.Length; i++)
-            {
-                listaStrings.Add(RleEncoding.Compress(array[i]));
-            }
-            //Console.WriteLine(txtlines[0].ToString());
-            //Console.WriteLine();
-            //Console.WriteLine(AT.GetTxt(txtlines[0]));
+            Instructions();
+            Console.Clear();
+            Console.WriteLine("                                             Welcome to RLE COMPRESSOR V 1.1");
+            Console.WriteLine();
+            Console.Write("RLE> ");
+            filePath = Console.ReadLine();
+            string[] array = Compress.CompressAllLines(UF.OpenFileString(filePath));
             Console.ReadKey();
 
+        }
+        
+        private static void Instructions()
+        {
+            Console.WriteLine("                                             Welcome to RLE COMPRESSOR V 1.1");
+            Console.WriteLine("                                                    -INSTRUCTIONS-");
+            Console.WriteLine();
+            Console.WriteLine("1) Compress: To compress a file follow these steps:");
+            Console.WriteLine("1.1)Write in RLE level: rle.exe -c -f(directory where the file is saved)");
+            Console.WriteLine("1.2)Press Enter");
+            Console.WriteLine("1.3)Search your file in the same directory with .rlex at the end.");
+            Console.WriteLine("1.4)Enjoy the statistics.");
+            Console.WriteLine();
+            Console.WriteLine("2) Decompress: To decompress a file follow these steps:");
+            Console.WriteLine("2.1)Write in RLE level: rle.exe -d -f(directory where the file is saved).rlex");
+            Console.WriteLine("2.2)Press Enter");
+            Console.WriteLine("2.3)Search your file in the same directory with the original extension at the start.");
+            Console.WriteLine();
+            Console.WriteLine("PRESS ENTER TO CONTINUE!");
+            Console.ReadKey();
         }
     }
 }
